b80c59ef7dedd8c34890dfea9336933fe943c96e
Ed Stephinson <edward.stephinson@gmail.com>
2024-03-09 15:44:29 +0000

    Subscribe to events via a handler

---
+++ examples/subscribe/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sailhouse/sdk-go/sailhouse"
+)
+
+func main() {
+	token := os.Getenv("SAILHOUSE_TOKEN")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	client := sailhouse.NewSailhouseClient(token)
+	client.Subscribe(ctx, "example-topic", "example-subscription", func(ctx context.Context, e *sailhouse.Event) {
+		message := fmt.Sprintf("Received event: %s", e.ID)
+
+		var data map[string]interface{}
+		err := e.As(&data)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		message += fmt.Sprintf("\nData: %v", data)
+
+		fmt.Println(message)
+
+		err = e.Ack(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}, &sailhouse.SubscriptionOptions{
+		OnError: func(err error) {
+			fmt.Println(err)
+			cancel()
+		},
+		ExitOnErr: true,
+	})
+
+	select {
+	case <-ctx.Done():
+		return
+	}
+}
